b9f9008ae894bc7e601d65fa822eb1c6419cf695
AGPragadeesh28 <72847378+AGPragadeesh28@users.noreply.github.com>
2020-10-15 10:42:48 -0700

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,46 @@
+<script>
+       
+           function validation()
+           {
+
+                                         if(firstname = = " ")
+                                       
+                                        {
+
+                                          alert("please enter the firstname );
+
+                                         }
+
+                                         if(lastname = = " ")
+
+                                         {
+
+                                          alert("please enter the lastname );
+
+                                          return false;
+
+                                         }
+ 
+                                          if(Emailid !=  '@'  || Emailid != '.')
+                                          
+                                         {
+              
+                                          alert("You have entered an invalid email address!");
+
+                                          return false;
+              
+                                          }
+
+                                         if (gender != "male" && gender != "female") 
+
+                                         {
+
+                                           alert("Atleast one Gender should chose");
+     
+                                         }
+
+          }
+
+</script>
+                                  
+                                        
